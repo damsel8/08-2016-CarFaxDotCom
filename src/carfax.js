948325d4e80bcf571660ecc0d12d29ad73e7fe77
Erica <ericastephan@gmail.com>
2016-08-31 20:56:30 -0700

    finished through YearParser

--- src/carfax.js
+++ src/carfax.js
@@ -1,5 +1,6 @@
-var database = database.carDataBase
+var database = database.carDataBase;
 var carData = database.carData;
+
 //console.log(carData);
 // your code here
 
@@ -16,22 +17,16 @@ toolbelt.transform(testArray, function(element){
 //model:  target list data is an ARRAY of prices of the cars
 //inputs = an array of objects that has 6 properties each.  outputs = an array of numbers (prices) of cars
 
-var inputData = [{id:100, year:2000, make:'Ford', model:'Focus', color:'blue', price:2000   }, {id:100, year:2000, make:'Ford', model:'Focus', color:'blue', price:2000   }]
+/*var inputData = [{id:100, year:2000, make:'Ford', model:'Focus', color:'blue', price:2000   }, {id:100, year:2000, make:'Ford', model:'Focus', color:'blue', price:2000   }]
 
-var outputData = [2000, 10000, 20000, 6200]
-
-
+var outputData = [2000, 10000, 20000, 6200]*/
 
 //pseudocode:
 
 var priceParser = function(cars){
 
-
-
 // Create a new empty array called "outputData"
 var outputData = [];
-
-
 // Iterate thru an array of objects.  
  toolbelt.transform(cars, function(carObject){
  	toolbelt.loop(carObject,function(value, key){
@@ -41,25 +36,36 @@ var outputData = [];
  	})
 
  })
-
-
 		//For each object, loop thru the properties and 
 		// test if key ==== price
 		// If the key is price, add value of price to the outputData we created.
 //REturn our outputData array we created
-
-
-
-
 return outputData;
-
 };
+
+console.log(priceParser(carData));
 
 //TOOLS WE WANT TO USE: 
 //1) transform for outer loop
 //2) loop for the inner loop
 //3) console log for testing
 
+var yearParser = function(cars){
+	var yearCollection=[];
+	toolbelt.transform(cars, function(carObject){
+		toolbelt.loop(carObject, function(value, key){
+			if (key==='year'){
+				yearCollection.push(value);
+			}
+
+		})
+})
+return yearCollection;
+};
+var testArray =[1,2,34]
+database.currentYears = yearParser(carData);
+database.test = testArray
+console.log(database.currentYears);
 
 
 
